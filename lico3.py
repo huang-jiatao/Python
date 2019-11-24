fc87822216176eb0bd694d25ec16c2eef17a698a
huang-jiatao <huang_jia_tao@163.com>
2019-11-25 00:26:11 +0800

    '力扣算法3' git commit -m l

---
+++ lico3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        st = {}
+        i, ans = 0, 0
+        for j in range(len(s)):
+            if s[j] in st:
+                i = max(st[s[j]], i)
+            ans = max(ans, j - i + 1)
+            st[s[j]] = j + 1
+        print(ans)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    length = s.lengthOfLongestSubstring("abcabcbb")
