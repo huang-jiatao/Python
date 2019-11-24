95f880c90847801dd148b923eea9e04552647c43
huang-jiatao <huang_jia_tao@163.com>
2019-11-25 00:42:54 +0800

    课程数据库sqlite

---
+++ test11.py
@@ -0,0 +1,35 @@
+import sqlite3
+'''
+conn = sqlite3.connect('test.db')
+c = conn.cursor()
+print("Opened database successfully")
+
+c.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (1, 'Paul', 32, 'California', 20000.00 )")
+
+c.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (2, 'Allen', 25, 'Texas', 15000.00 )")
+
+c.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 )")
+
+c.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \
+      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 )")
+
+conn.commit()
+print("Records created successfully")
+conn.close()
+'''
+conn = sqlite3.connect('test.db')
+c = conn.cursor()
+print("Opened database successfully")
+
+cursor = c.execute("SELECT id, name, address, salary  from COMPANY")
+for row in cursor:
+    print("ID = ", row[0])
+    print("NAME = ", row[1])
+    print("ADDRESS = ", row[2])
+    print("SALARY = ", row[3], "\n")
+
+print("Operation done successfully")
+conn.close()
